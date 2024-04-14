2311bb922c371ab5150c7a108b9d8783cfc13e84
Tobias Prima <primatjahja@gmail.com>
2024-04-14 23:48:18 +0800

    feat: home screen

---
+++ lib/app_view.dart
@@ -0,0 +1,22 @@
+import 'package:expense_tracker/screens/home/views/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Expense Tracker",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.light(
+              background: Colors.grey.shade100,
+              onBackground: Colors.black,
+              primary: const Color(0xFF00B2B7),
+              secondary: const Color(0xFFE064F7),
+              tertiary: const Color(0xFFFF8D6C)),
+        ),
+        home: const HomeScreen());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
+import 'package:expense_tracker/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
-  }
 }
