05beee9fa84d5dbc88b3debfbb63f88df2fe4118
Tobias Prima <primatjahja@gmail.com>
2024-04-17 22:56:37 +0800

    feat: stats page

---
+++ lib/screens/stats/stats.dart
@@ -0,0 +1,32 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class StatScreen extends StatelessWidget {
+  const StatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Transaction',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              child: BarChart(BarChartData()),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
