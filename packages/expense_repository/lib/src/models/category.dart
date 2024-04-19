080024786cdb0203816cc705bfbc592054df3009
Tobias Prima <primatjahja@gmail.com>
2024-04-19 19:45:22 +0800

    feat: function to convert class to entity

--- packages/expense_repository/lib/src/models/category.dart
+++ packages/expense_repository/lib/src/models/category.dart
@@ -12,4 +12,32 @@ class Category {
     required this.icon,
     required this.color,
   });
+
+  static final empty = Category(
+    categoryId: '',
+    name: '',
+    totalExpenses: 0,
+    icon: '',
+    color: '',
+  );
+
+  CategoryEntity toEntity() {
+    return CategoryEntity(
+      categoryId: categoryId,
+      name: name,
+      totalExpenses: totalExpenses,
+      icon: icon,
+      color: color,
+    );
+  }
+
+  static Category fromEntity(CategoryEntity entity) {
+    return Category(
+      categoryId: entity.categoryId,
+      name: entity.name,
+      totalExpenses: entity.totalExpenses,
+      icon: entity.icon,
+      color: entity.color,
+    );
+  }
 }
