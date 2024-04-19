70fdba799fd9c1669f697720366793b2619a30ed
Tobias Prima <primatjahja@gmail.com>
2024-04-19 19:41:04 +0800

    feat: category class

---
+++ packages/expense_repository/lib/src/models/category.dart
@@ -0,0 +1,15 @@
+class Category {
+  String categoryId;
+  String name;
+  int totalExpenses;
+  String icon;
+  String color;
+
+  Category({
+    required this.categoryId,
+    required this.name,
+    required this.totalExpenses,
+    required this.icon,
+    required this.color,
+  });
+}
