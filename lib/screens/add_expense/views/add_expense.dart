6c1e9637a9c0ed96ca1fa4a3a4dc5cdd9794838b
Tobias Prima <primatjahja@gmail.com>
2024-04-22 21:33:09 +0800

    feat: bloc builder to wrap all

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -34,160 +34,166 @@ class _AddExpenseState extends State<AddExpense> {
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.background,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Add Expenses',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: TextFormField(
-                  controller: expenseController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    prefixIcon: const Icon(
-                      FontAwesomeIcons.dollarSign,
-                      size: 16,
-                      color: Colors.grey,
+        body: BlocBuilder<GetCategoriesBloc, GetCategoriesState>(
+          builder: (context, state) {
+            if (state is GetCategoriesSuccess) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Add Expenses',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
                     ),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
-              TextFormField(
-                readOnly: true,
-                onTap: () {},
-                controller: categoryController,
-                textAlignVertical: TextAlignVertical.center,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: const Icon(
-                    FontAwesomeIcons.list,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      getCategoryCreation(context);
-                    },
-                    icon: const Icon(
-                      FontAwesomeIcons.plus,
-                      size: 16,
-                      color: Colors.grey,
+                    const SizedBox(
+                      height: 16,
                     ),
-                  ),
-                  hintText: 'Category',
-                  border: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(12))),
-                ),
-              ),
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:
-                        BorderRadius.vertical(bottom: Radius.circular(12))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: BlocBuilder<GetCategoriesBloc, GetCategoriesState>(
-                    builder: (context, state) {
-                      if (state is GetCategoriesSuccess) {
-                        return ListView.builder(
-                          itemBuilder: (context, int i) {
-                            return Card(
-                                child: ListTile(
-                              leading: Image.asset(
-                                  'assets/${state.categories[i].icon}.png',
-                                  scale: 2),
-                              title: const Text('Food'),
-                              tileColor: Colors.red,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8)),
-                            ));
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      child: TextFormField(
+                        controller: expenseController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: const Icon(
+                            FontAwesomeIcons.dollarSign,
+                            size: 16,
+                            color: Colors.grey,
+                          ),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    TextFormField(
+                      readOnly: true,
+                      onTap: () {},
+                      controller: categoryController,
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: const Icon(
+                          FontAwesomeIcons.list,
+                          size: 16,
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () async {
+                            var newCategory =
+                                await getCategoryCreation(context);
+                            setState(() {
+                              state.categories.insert(0, newCategory);
+                            });
                           },
-                          itemCount: state.categories.length,
-                        );
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                controller: dateController,
-                textAlignVertical: TextAlignVertical.center,
-                readOnly: true,
-                onTap: () async {
-                  DateTime? newDate = await showDatePicker(
-                      context: context,
-                      initialDate: selectedDate,
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime.now().add(const Duration(days: 365)));
+                          icon: const Icon(
+                            FontAwesomeIcons.plus,
+                            size: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        hintText: 'Category',
+                        border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(12))),
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.vertical(
+                              bottom: Radius.circular(12))),
+                      child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListView.builder(
+                            itemBuilder: (context, int i) {
+                              return Card(
+                                  child: ListTile(
+                                leading: Image.asset(
+                                    'assets/${state.categories[i].icon}.png',
+                                    scale: 2),
+                                title: Text(state.categories[i].name),
+                                tileColor: Color(state.categories[i].color),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                              ));
+                            },
+                            itemCount: state.categories.length,
+                          )),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextFormField(
+                      controller: dateController,
+                      textAlignVertical: TextAlignVertical.center,
+                      readOnly: true,
+                      onTap: () async {
+                        DateTime? newDate = await showDatePicker(
+                            context: context,
+                            initialDate: selectedDate,
+                            firstDate: DateTime.now(),
+                            lastDate:
+                                DateTime.now().add(const Duration(days: 365)));
 
-                  if (newDate != null) {
-                    setState(() {
-                      dateController.text =
-                          DateFormat('dd/MM/yyyy').format(newDate);
-                      selectedDate = newDate;
-                    });
-                  }
-                  ;
-                },
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: const Icon(
-                    FontAwesomeIcons.clock,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                  hintText: 'Date',
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(12)),
+                        if (newDate != null) {
+                          setState(() {
+                            dateController.text =
+                                DateFormat('dd/MM/yyyy').format(newDate);
+                            selectedDate = newDate;
+                          });
+                        }
+                        ;
+                      },
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: const Icon(
+                          FontAwesomeIcons.clock,
+                          size: 16,
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Date',
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(12)),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    SizedBox(
+                        width: double.infinity,
+                        height: kToolbarHeight,
+                        child: TextButton(
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12))),
+                            onPressed: () {},
+                            child: const Text(
+                              'Save',
+                              style:
+                                  TextStyle(fontSize: 22, color: Colors.white),
+                            )))
+                  ],
                 ),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
-              SizedBox(
-                  width: double.infinity,
-                  height: kToolbarHeight,
-                  child: TextButton(
-                      style: TextButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12))),
-                      onPressed: () {},
-                      child: const Text(
-                        'Save',
-                        style: TextStyle(fontSize: 22, color: Colors.white),
-                      )))
-            ],
-          ),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
     );
