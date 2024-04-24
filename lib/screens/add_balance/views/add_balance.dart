6a4a8b5c91266a83c743a1fac35cc335d9a6b9af
Tobias Prima <primatjahja@gmail.com>
2024-04-24 21:28:54 +0800

    feat: routing to add balance page

---
+++ lib/screens/add_balance/views/add_balance.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddBalance extends StatelessWidget {
+  const AddBalance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
