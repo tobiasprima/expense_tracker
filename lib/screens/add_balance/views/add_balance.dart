69ac361b4f4ab6d5b186fc276e943f38e55689e4
Tobias Prima <primatjahja@gmail.com>
2024-04-24 22:01:12 +0800

    feat: add balance page

--- lib/screens/add_balance/views/add_balance.dart
+++ lib/screens/add_balance/views/add_balance.dart
@@ -1,10 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AddBalance extends StatelessWidget {
   const AddBalance({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Add Income',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: TextFormField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.dollarSign,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            TextFormField(
+              readOnly: true,
+              onTap: () {},
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(FontAwesomeIcons.list),
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      FontAwesomeIcons.plus,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  hintText: 'Category',
+                  border: const OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(12)))),
+            ),
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(12),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemBuilder: (context, int i) {
+                    return Card(
+                      child: ListTile(
+                        onTap: () {},
+                        leading: Icon(
+                          FontAwesomeIcons.plus,
+                          size: 12,
+                        ),
+                        title: Text('Category'),
+                        tileColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: 3,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              textAlignVertical: TextAlignVertical.center,
+              readOnly: true,
+              onTap: () {
+                showDatePicker(
+                    context: context,
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(const Duration(days: 365)));
+              },
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: const Icon(
+                  FontAwesomeIcons.clock,
+                  size: 16,
+                  color: Colors.grey,
+                ),
+                hintText: 'Date',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: kToolbarHeight,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Save',
+                  style: TextStyle(fontSize: 22, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
