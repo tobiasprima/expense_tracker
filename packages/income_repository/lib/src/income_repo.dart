a6495901107050f182fd5813785aadc25853a6b3
Tobias Prima <primatjahja@gmail.com>
2024-04-26 15:20:03 +0800

    feat: create income category bloc

--- packages/income_repository/lib/src/income_repo.dart
+++ packages/income_repository/lib/src/income_repo.dart
@@ -1,1 +1,5 @@
-abstract class IncomeRepository {}
+import 'package:income_repository/income_repository.dart';
+
+abstract class IncomeRepository {
+  Future<void> createIncomeCategory(IncomeCategory category);
+}
